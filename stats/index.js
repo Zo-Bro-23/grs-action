5f171af44949da721f1a0ffd21e58bc5ff5c1ea2
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-10 09:10:45 +0000

    Test

--- stats/index.js
+++ stats/index.js
@@ -3,12 +3,16 @@ import * as github from '@actions/github'
 import * as githubReadmeStats from '@zo-bro-23/github-readme-stats-test'
 import * as fs from 'fs'
 
-const token = core.getInput('token', {required: true})
-const username = core.getInput('username', {required: true})
+try {
+    const token = core.getInput('token', { required: true })
+    const username = core.getInput('username', { required: true })
 
-process.env.token = token
+    process.env.token = token
 
-githubReadmeStats.generateStatsCard({ username })
-    .then(card => {
-        fs.writeFileSync('card.svg', card)
-    })
+    githubReadmeStats.generateStatsCard({ username, theme: core.getInput('theme') })
+        .then(card => {
+            fs.writeFileSync('card.svg', card)
+        })
+} catch (error) {
+    core.setFailed(error.message)
+}

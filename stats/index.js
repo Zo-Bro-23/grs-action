0996359eb77f8743800395a696304e744cc0139a
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-11 10:05:33 +0000

    Finish stat card

--- stats/index.js
+++ stats/index.js
@@ -4,12 +4,37 @@ import * as githubReadmeStats from '@zo-bro-23/github-readme-stats-test'
 import * as fs from 'fs'
 
 try {
-    const token = core.getInput('token', { required: true })
-    const username = core.getInput('username', { required: true })
+    const options = {
+        username: core.getInput('username', { required: true }),
+        theme: core.getInput('theme'),
+        show_icons: core.getInput('show_icons'),
+        count_private: core.getInput('count_private'),
+        include_all_commits: core.getInput('include_all_commits'),
+        hide: core.getInput('hide'),
+        hide_title: core.getInput('hide_title'),
+        hide_border: core.getInput('hide_border'),
+        line_height: core.getInput('line_height'),
+        title_color: core.getInput('title_color'),
+        icon_color: core.getInput('icon_color'),
+        text_color: core.getInput('text_color'),
+        bg_color: core.getInput('bg_color'),
+        cache_seconds: core.getInput('cache_seconds'),
+        custom_title: core.getInput('custom_title'),
+        disable_animations: core.getInput('disable_animations'),
+        locale: core.getInput('locale'),
+        hide_rank: core.getInput('hide_rank'),
+        border_radius: core.getInput('border_radius'),
+        border_color: core.getInput('border_color'),
+        text_bold: core.getInput('text_bold'),
+        card_width: core.getInput('card_width'),
+        ring_color: core.getInput('ring_color'),
+        exclude_repo: core.getInput('exclude_repo')
+    }
 
-    process.env['PAT_1'] = token
+    process.env['PAT_1'] = core.getInput('token', { required: true })
+    process.env['FETCH_MULTI_PAGE_STARS'] = core.getInput('fetch_multipage')
 
-    githubReadmeStats.generateStatsCard({ username, theme: core.getInput('theme') })
+    githubReadmeStats.generateStatsCard(options)
         .then(card => {
             fs.writeFileSync('card.svg', card)
         })

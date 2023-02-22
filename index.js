9f6b9d4dd4af41d784c43020b6fb836c833542d4
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-22 06:27:31 +0000

    Big change to query params

---
+++ index.js
@@ -0,0 +1,45 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as githubReadmeStats from '@zo-bro-23/github-readme-stats-test'
+import * as fs from 'fs'
+import queryString from 'query-string'
+
+try {
+    const options = queryString.parse(core.getInput('options', { required: true }))
+    const card = core.getInput('card', { required: true })
+    const path = core.getInput('path')
+
+    process.env['PAT_1'] = core.getInput('token', { required: true })
+    process.env['FETCH_MULTI_PAGE_STARS'] = core.getInput('fetch_multipage')
+
+    switch (card) {
+        case '':
+            githubReadmeStats.generateStatsCard(options)
+                .then(card => {
+                    fs.writeFileSync(path || 'grs/stats.svg', card)
+                })
+            break
+        case 'repo':
+            githubReadmeStats.generateRepoCard(options)
+                .then(card => {
+                    fs.writeFileSync(path || 'grs/repo.svg', card)
+                })
+            break
+        case 'top-langs':
+            githubReadmeStats.generateTopLanguagesCard(options)
+                .then(card => {
+                    fs.writeFileSync(path || 'grs/top-langs.svg', card)
+                })
+            break
+        case 'wakatime':
+            githubReadmeStats.generateWakatimeCard(options)
+                .then(card => {
+                    fs.writeFileSync(path || 'grs/wakatime.svg', card)
+                })
+        default:
+            throw new Error('Card type must be `repo`, `top-langs`, or `wakatime`')
+            break
+    }
+} catch (error) {
+    core.setFailed(error.message)
+}
--- stats/node_modules/@zo-bro-23/github-readme-stats-test/src/cards/index.js
+++
@@ -1,4 +0,0 @@
-export { renderRepoCard } from "./repo-card.js";
-export { renderStatsCard } from "./stats-card.js";
-export { renderTopLanguages } from "./top-languages-card.js";
-export { renderWakatimeCard } from "./wakatime-card.js";

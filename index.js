9503e61278333a93ce5d6e12eed914d25aa151a0
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-22 07:06:43 +0000

    Fix

--- index.js
+++ index.js
@@ -3,38 +3,41 @@ import * as github from '@actions/github'
 import * as githubReadmeStats from '@zo-bro-23/github-readme-stats-test'
 import * as fs from 'fs'
 import queryString from 'query-string'
+import * as path from 'path'
 
 try {
     const options = queryString.parse(core.getInput('options', { required: true }))
     const card = core.getInput('card', { required: false })
-    const path = core.getInput('path')
+    const file = core.getInput('path')
 
     process.env['PAT_1'] = core.getInput('token', { required: true })
     process.env['FETCH_MULTI_PAGE_STARS'] = core.getInput('fetch_multipage')
+
+    fs.mkdirSync(path.dirname(file), { recursive: true })
 
     switch (card) {
         case '':
             githubReadmeStats.generateStatsCard(options)
                 .then(card => {
-                    fs.writeFileSync(path || 'grs/stats.svg', card)
+                    fs.writeFileSync(file || 'grs/stats.svg', card)
                 })
             break
         case 'repo':
             githubReadmeStats.generateRepoCard(options)
                 .then(card => {
-                    fs.writeFileSync(path || 'grs/repo.svg', card)
+                    fs.writeFileSync(file || 'grs/repo.svg', card)
                 })
             break
         case 'top-langs':
             githubReadmeStats.generateTopLanguagesCard(options)
                 .then(card => {
-                    fs.writeFileSync(path || 'grs/top-langs.svg', card)
+                    fs.writeFileSync(file || 'grs/top-langs.svg', card)
                 })
             break
         case 'wakatime':
             githubReadmeStats.generateWakatimeCard(options)
                 .then(card => {
-                    fs.writeFileSync(path || 'grs/wakatime.svg', card)
+                    fs.writeFileSync(file || 'grs/wakatime.svg', card)
                 })
         default:
             throw new Error('Card type must be `repo`, `top-langs`, or `wakatime`')
